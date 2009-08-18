6f40edf7d8e208e0eed5ae7433dd3727300032a9
Romuald Brunet <romuald@chivil.com>
2009-08-18 11:23:08 +0200

    better parser

--- parser.pl
+++ parser.pl
@@ -3,17 +3,24 @@ use warnings;
 
 use Data::Dumper;
 
-my $string = q|
-# mdfind queries
-include-query = kMDItemFinderComment = 'backup'w
+my $string = q|# mdfind queries
+include-query = kMDItemFinderComment = 'backup'w    
 exclude-query = kMDItemFinderComment = 'nobckup'w
+
+dummy = dumb # This is an inline comment, must be preceded by a space 
 
 # Where am I?
 backup-to = /Volumes/Backup\ HD/XXXXXX-CHANGEME
 
+# Paths to include that aren't found by mdfind
 @include-paths = ()
+
+# Paths to exclude that aren't found by mdfind
 @exclude-paths = (
 	/Users/*/Library/Caches
+
+    # Spaces must be escaped too, see below
+    # /Library/Application\ Support
 )
 
 @rsync-options = (
@@ -23,15 +30,18 @@ backup-to = /Volumes/Backup\ HD/XXXXXX-CHANGEME
 
 my %conf = ();
 
-$string =~ s/^\s*[#]+.*//gm;
+# Remove inline comments
+$string =~ s/(?<!\\)\s+#.*//gm;
 
-while ( $string =~ /^([^\@\s]+?)\s*=\s*(.*)/gm  ) {
+# and unescape the non-comments
+$string =~ s/\\#/#/gm;
+
+while ( $string =~ /^\s*([^#\@\s]+?)\s*=\s*(.*?)\s*$/gm  ) {
 	$conf{$1} = $2;
 }
 
-while ( $string =~ /\@(\S+?)\s*=\s*\((.*?)\)/gs ) {
-    my @values = grep { length } split /\s+/s, $2;
-
+while ( $string =~ /\@(\S+?)\s*=\s*\((.*?)(?<!\\)\)/gs ) {
+    my @values = map { s/\\([ )])/$1/g; $_ } grep { length } split /(?<!\\)\s+/s, $2;
     $conf{$1} = [ @values ];
 }
 print Dumper \%conf;
