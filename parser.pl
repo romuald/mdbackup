26924a4892705a3cb9cc847a1172e2b82854592f
Romuald Brunet <romuald@chivil.com>
2009-08-22 18:34:04 +0200

    getopts working + more comments

--- parser.pl
+++ parser.pl
@@ -1,7 +1,11 @@
 use strict;
 use warnings;
 
+use Fcntl qw/SEEK_SET/;
+use Getopt::Long;
+
 use Data::Dumper;
+
 
 =head1 load_config
 
@@ -12,18 +16,41 @@ TODO getopts to force options
 sub load_config() {
 	my %return = ();
 	
-	my $config_file =  glob("~/.mdbackuprc");
+	my $config_file =  "~/.mdbackuprc";
+
+	# setup conf using command line arguments
+	GetOptions(\%return, qw/
+		config=s
+		rsync-options=s@   backup-to=s
+		include-paths=s@   exclude-paths=s@
+		include-query=s    exclude-query=s	
+	/);
 	
-	# TODO be able to use a different location using command line
-	if ( -f $config_file && -r _ ) {
-		open(CONF, $config_file);
-	} else {
-		*CONF = \*DATA;
+	# lazy split using comma for rsync options
+	foreach (("rsync-options")) {
+		next unless ref $return{$_};
+		$return{$_} = [ split /,/, $return{$_}->[0] ];
 	}
 	
-	my $strconf = join "", <CONF>;
-	close(CONF);
-		
+	# was config given as part of the command line ?
+	$return{config} ||= $config_file;
+	$config_file = glob(delete $return{config});
+	
+	my $strconf = undef;
+	
+	# Try to read config file, otherwise revert to internal defaults
+	if ( -f $config_file && -r _ ) {
+		open CONF, $config_file;
+		$strconf = join "", <CONF>;
+		close CONF;
+	} else {
+		# print STDERR "Can't open config file $config_file, revert to defaults\n";
+		# Rewind data handle after reading, in case we'll need to read it again
+		my $origin = tell(DATA);
+		$strconf = join "", <DATA>;
+		seek(DATA, $origin, SEEK_SET);
+	}
+	
 	# Remove inline comments
 	$strconf =~ s/(?<!\\)\s+#.*//gm;
 
@@ -32,11 +59,15 @@ sub load_config() {
 
 	# Simple scalars, allow empty values with "varname = "
 	while ( $strconf =~ /^[\040\t]*([^#\@\s]+?)[\040\t]*=[\040\t]*(.*?)[\040\t]*$/gm  ) {
+		next if defined($return{$1});
+		
 		$return{$1} = $2;
 	}
 
 	# Arrays
 	while ( $strconf =~ /\@(\S+?)\s*=\s*\((.*?)(?<!\\)\)/gs ) {
+		next if defined($return{$1});
+		
 	    my @values =
 	    # 3. then unescape spaces and parenthesis
 	    map { s/\\([ )])/$1/g; $_ }
@@ -45,7 +76,7 @@ sub load_config() {
 	    # 1. split using non-escaped whitespaces
 	    split /(?<!\\)\s+/s, $2;
 
-	    $return{$1} = \@values;
+	    $return{$1} = \@values
 	}
 	# and no dict yet
 	
